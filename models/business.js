a9d3006d1919622c7356c9d30e89f1d25f13bae6
sidneyramos <sidney.ramos95@gmail.com>
2017-05-18 23:29:53 +1000

    Added menu onto Business model

--- models/business.js
+++ models/business.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var BusinessSchema = new Schema({
   user_id: {type: String},
   name: {type: String},
-  ABN: {type: String}
+  ABN: {type: String},
+  menu: []
 });
 
 module.exports = mongoose.model("Business", BusinessSchema);
