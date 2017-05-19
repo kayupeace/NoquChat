91b7f1c4463382c0ffac44f0d19c96f1f5c88e6d
Sidney Ramos <sidney.ramos95@gmail.com>
2017-05-19 10:14:10 +1000

    temp error fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var mongoose = require("mongoose");
 
 var db = mongoose.connect(process.env.MONGODB_URI);
 var Business = require("./models/business");
-var Session = require("./models/session")
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
