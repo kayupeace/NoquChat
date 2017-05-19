829c6ae75381441a05fe47fad2f258e0d59fb89a
Sidney Ramos <sidney.ramos95@gmail.com>
2017-05-19 10:19:12 +1000

    Fixed Session model error

--- models/session.js
+++ models/session.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var SessionSchema = new Schema({
   user_id: {type: String},
   business: {
-    business_id: {type: String}
+    business_id: {type: String},
     name: {type: String},
     ABN: {type: String},
     menu: []
