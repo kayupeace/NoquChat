3ab0d7a1947964a18ca4314ba5781e2ac90ea87b
sidneyramos <sidney.ramos95@gmail.com>
2017-05-20 12:18:58 +1000

    Trial button

--- app.js
+++ app.js
@@ -258,6 +258,40 @@ function findBusiness(userId, message){
             }
             // strMessage += businesses ;
             sendMessage(userId, {text: strMessage});  
+            sendMessage(userId, {
+                attachment:{
+                  type:"template",
+                  payload:{
+                    template_type:"generic",
+                    elements:[
+                       {
+                        title:"Welcome to Peter\'s Hats",
+                        image_url:"https://petersfancybrownhats.com/company_image.png",
+                        subtitle:"We\'ve got the right hat for everyone.",
+                        default_action: {
+                          type: "web_url",
+                          url: "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                          messenger_extensions: true,
+                          webview_height_ratio: "tall",
+                          fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                        },
+                        buttons:[
+                          {
+                            type:"web_url",
+                            url:"https://petersfancybrownhats.com",
+                            title:"View Website"
+                          },
+                          {
+                            type:"postback",
+                            title:"Start Chatting",
+                            payload:"DEVELOPER_DEFINED_PAYLOAD"
+                          }              
+                        ]      
+                      }
+                    ]
+                  }
+                }
+            });
         }
         else{
           sendMessage(userId, {text: "Cannot find business. Try again"});
