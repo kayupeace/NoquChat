2450b670af15ce7f5e68f4cfc8eb1a954d9cca88
Kevin Yu <kevinyu@Kevins-MacBook-Pro.local>
2017-10-06 13:57:14 +1100

    Front-end: password display and hide javascript, add but not active front-end email enter format check

--- public/assets/js/noqubot.js
+++ public/assets/js/noqubot.js
@@ -9,3 +9,27 @@ $(document).ready(function(){
     });
      **/
 });
+
+function show(){
+    'use strict';
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'text');
+    document.getElementById('eye').className = 'fa fa-eye-slash';
+}
+function hide() {
+    'use strict';
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'password');
+    document.getElementById('eye').className = 'fa fa-eye';
+}
+var isShow = 0;
+function showHidePassword() {
+    'use strict';
+    if (isShow === 0) {
+        isShow = 1;
+        show();
+    } else {
+        isShow = 0;
+        hide();
+    }
+}
