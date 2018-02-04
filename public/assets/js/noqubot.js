b9e52be6b31e81eace55da0ee8e7fd74f41c6bf8
kaibin <kaibin@kaibins-MacBook-Pro.local>
2018-02-04 17:52:48 +1100

    View: Update New Boostrap Style, complete with base, login, registration page

--- public/assets/js/noqubot.js
+++ public/assets/js/noqubot.js
@@ -1,14 +1,3 @@
-$(document).ready(function(){
-    $(".button-collapse").sideNav({
-        edge: 'right'
-    });
-
-    /**
-    $('[name="facebook-login-btn"]').click(function() {
-        window.open("/auth/facebook", "PopupWindow", "width=475,height=268");
-    });
-     **/
-});
 
 function show(){
     'use strict';
@@ -33,3 +22,30 @@ function showHidePassword() {
         hide();
     }
 }
+
+$('#id_password').on('keyup', function() {
+    var a = $("#id_password").val();
+    var b = $("#id_confirm_password").val();
+    if(a == b && a != ''){
+        $("#submit_form").removeAttr('disabled');
+        $("#my-hint").replaceWith("<div id='my-hint' style='color:#29a745;'>😊 Match !!!</div>");
+        $("#submit_form").removeAttr('title');
+    }else {
+        $("#submit_form").attr('disabled','disabled');
+        $("#my-hint").replaceWith("<div id='my-hint' style='color:red;'>😢 Your Password does not match</div>");
+        $("#submit_form").attr('title','filed, confirm password and password should be same');
+    }
+});
+$('#id_confirm_password').on('keyup', function() {
+    var a = $("#id_password").val();
+    var b = $("#id_confirm_password").val();
+    if(a == b && a!=''){
+        $("#submit_form").removeAttr('title');
+        $("#submit_form").removeAttr('disabled');
+        $("#my-hint").replaceWith("<div id='my-hint' style='color:#29a745;'>😊Match !!!</div>");
+    }else {
+        $("#submit_form").attr('disabled','disabled');
+        $("#my-hint").replaceWith("<div id='my-hint' style='color:red;'>😢Your Password does not match</div>");
+        $("#submit_form").attr('title','confirm password and password should be same');
+    }
+});
